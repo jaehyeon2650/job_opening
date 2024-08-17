42b7c9edbb2e61cd2b683a1cc07a02f9ccd6d27a
wogus <ind07152@naver.com>
2024-08-17 17:38:53 +0900

    Initial commit

---
+++ src/main/java/project/general_project/GeneralProjectApplication.java
@@ -0,0 +1,13 @@
+package project.general_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/general_project/GeneralProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package project.general_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
