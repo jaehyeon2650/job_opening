818af9bfdcb026d1f401559b3f274f841888f8c3
wogus <ind07152@naver.com>
2024-08-18 00:00:36 +0900

    fix: 스프링 시큐리티 기본 화면 없애기

--- src/main/java/project/general_project/GeneralProjectApplication.java
+++ src/main/java/project/general_project/GeneralProjectApplication.java
@@ -2,8 +2,9 @@ package project.general_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class GeneralProjectApplication {
 
 	public static void main(String[] args) {
