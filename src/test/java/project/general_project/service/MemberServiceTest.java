6325acf723c2b3b4c75a777276f5fb9e9c1fdaa2
wogus <ind07152@naver.com>
2024-08-17 23:57:25 +0900

    test: 비밀번호 해쉬, 중복 아이디 검증

--- src/test/java/project/general_project/service/MemberServiceTest.java
+++ src/test/java/project/general_project/service/MemberServiceTest.java
@@ -5,12 +5,15 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import project.general_project.domain.Address;
 import project.general_project.domain.Member;
 import project.general_project.repository.member.MemberRepository;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 @Transactional
 class MemberServiceTest {
@@ -32,4 +35,29 @@ class MemberServiceTest {
         Member findMember = memberService.findById(member.getId());
         assertThat(findMember).isEqualTo(member);
     }
+    @Test
+    public void 같은_아이디() throws Exception{
+        //given
+        Member member1=Member.createMember("asd","asd","asd",Address.createAddress("asd","asd","asda"));
+        memberService.save(member1);
+        //when
+        Member member2=Member.createMember("asd","asd","asd",Address.createAddress("asd","asd","asda"));
+        Long saveId = memberService.save(member2);
+        //then
+        assertThat(saveId).isEqualTo(-1L);
+    }
+
+    @Test
+    public void 비밀번호_해쉬() throws Exception{
+        //given
+        PasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
+        Member member1=Member.createMember("asd","asd","asd",Address.createAddress("asd","asd","asda"));
+        memberService.save(member1);
+        //when
+        Member findMember = memberService.findById(member1.getId());
+        //then
+        boolean password = passwordEncoder.matches("asd",findMember.getPassword());
+        assertThat(password).isTrue();
+    }
+
 }
