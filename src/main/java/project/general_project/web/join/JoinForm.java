b2d1b2ad1406ec840f8f2376aba0e022a2dc622e
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-08-18 17:04:16 +0900

    Login and join validation (#2)
    * build: validation 추가
    
    * feat: 회원가입 검증 로직 추가
    
    * docs: 회원가입 검증 템플릿 추가
    
    * design: 회원가입 검증 문자 변경
    
    * feat: 회원가입 패스워드 검증 로직 추가
    
    * feat: 로그인 검증 구현
    
    * docs: 검증 관련 문자열 추가

--- src/main/java/project/general_project/web/join/JoinForm.java
+++ src/main/java/project/general_project/web/join/JoinForm.java
@@ -1,13 +1,25 @@
 package project.general_project.web.join;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class JoinForm {
+    @NotBlank
     private String username;
+    @NotBlank
     private String userId;
+    @NotBlank
     private String password;
+    private String passwordCheck;
+    @NotBlank
+    @Length(min = 6)
     private String zipcode;
+    @NotBlank
     private String city;
+    @NotBlank
     private String detailAddress;
 }
--- src/main/java/project/general_project/web/login/LoginForm.java
+++ src/main/java/project/general_project/web/login/LoginForm.java
@@ -1,9 +1,12 @@
 package project.general_project.web.login;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class LoginForm {
+    @NotBlank
     private String id;
+    @NotBlank
     private String password;
 }
