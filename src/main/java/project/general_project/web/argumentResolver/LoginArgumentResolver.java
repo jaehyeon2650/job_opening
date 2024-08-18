7b2b46378897ddc24100b6219745a97e69f6f5c6
장재현 <121144710+jaehyeon2650@users.noreply.github.com>
2024-08-18 18:51:22 +0900

    로그인 검증 기능 구현 (#3)
    * Initial commit2
    
    * Revert "Initial commit2"
    
    This reverts commit eff6a56feeb6260fb20e0c80617e0ac6ab22d139.
    
    * feat: add login html
    
    * feat: add home join html
    
    * feat: 회원 도메인 추가
    
    * feat: 회원 dto 추가
    
    * feat: 회원 레포지토리 추가
    
    * test: 회원 가입 테스트 추가
    
    * feat: 회원 서비스(로그인,회원가입) 추가
    
    * test: 회원 서비스(로그인,회원가입) 테스트 추가
    
    * feat: 회원 컨트롤러 추가
    
    * fix: 회원레포지토리 버그 수정
    
    * feat: 아이디로 회원 조회 추가
    
    * feat: 중복 아이디 검증과 비밀번호 해쉬 추가
    
    * test: 비밀번호 해쉬, 중복 아이디 검증
    
    * feat: 로그인 서비스 추가
    
    * test: 로그인 서비스 검증
    
    * feat: 로그인 컨트롤러 추가
    
    * docs: 로그인 html css 추가
    
    * design: 로그인 css
    
    * fix: 스프링 시큐리티 기본 화면 없애기
    
    * build: 스프링 시큐리티 추가
    
    * docs: 회원가입 css 추가
    
    * design: 회원가입 css
    
    * Initial commit2
    
    * build: validation 추가
    
    * feat: 회원가입 검증 로직 추가
    
    * docs: 회원가입 검증 템플릿 추가
    
    * design: 회원가입 검증 문자 변경
    
    * feat: 회원가입 패스워드 검증 로직 추가
    
    * feat: 로그인 검증 구현
    
    * docs: 검증 관련 문자열 추가
    
    * feat: 세션을 활용하여 로그인, 로그아웃 기능 구현
    
    * docs: 로그인 후 예비 화면
    
    * feat: 인터셉터, argumentResolver를 활용하여 로그인 여부 확인 기능 추가

---
+++ src/main/java/project/general_project/web/argumentResolver/LoginArgumentResolver.java
@@ -0,0 +1,32 @@
+package project.general_project.web.argumentResolver;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import project.general_project.domain.Member;
+import project.general_project.web.SessionConst;
+import project.general_project.web.login.Login;
+
+public class LoginArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasMemberType = Member.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation||hasMemberType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        HttpSession session = request.getSession(false);
+        if(session==null||session.getAttribute(SessionConst.LOGIN_MEMBER)==null){
+            return null;
+        }
+
+        return session.getAttribute(SessionConst.LOGIN_MEMBER);
+    }
+}
