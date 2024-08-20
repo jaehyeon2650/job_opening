9c0e4e6c39138e2e7141ab9e1d2e112a5ba6b3e4
wogus <ind07152@naver.com>
2024-08-20 17:54:38 +0900

    feat: postForm dto 생성

---
+++ src/main/java/project/general_project/web/postForm/PostForm.java
@@ -0,0 +1,39 @@
+package project.general_project.web.postForm;
+
+import jakarta.persistence.Lob;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import project.general_project.domain.RecruitmentStatus;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+public class PostForm {
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String content;
+    private RecruitmentStatus status;
+    private Long postId;
+    private String writer;
+    private LocalDateTime create;
+
+    public PostForm(String title, String content,RecruitmentStatus status,Long postId,String writer,LocalDateTime create) {
+        this.title = title;
+        this.content = content;
+        this.status=status;
+        this.postId=postId;
+        this.writer=writer;
+        this.create=create;
+    }
+
+    public PostForm(String title, String content,RecruitmentStatus status,Long postId) {
+        this.title = title;
+        this.content = content;
+        this.status=status;
+        this.postId=postId;
+
+    }
+}
