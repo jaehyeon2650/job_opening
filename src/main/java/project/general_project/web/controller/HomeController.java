cd4c94fd645fc786b87fa677a07cb8df4ae3990f
wogus <ind07152@naver.com>
2024-08-20 18:01:09 +0900

    feat: post 조회 추가

--- src/main/java/project/general_project/web/controller/HomeController.java
+++ src/main/java/project/general_project/web/controller/HomeController.java
@@ -6,14 +6,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import project.general_project.domain.Member;
+import project.general_project.domain.Post;
+import project.general_project.service.PostService;
 import project.general_project.web.SessionConst;
+import project.general_project.web.postForm.PostForm;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
+    private final PostService postService;
 
     @GetMapping("/")
     public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member member,Model model){
+        addPostFormToModel(model);
         if(member==null){
             return "home";
         }
@@ -21,12 +29,28 @@ public class HomeController {
         return "loginHome";
     }
 
+
     @GetMapping("/loginHome")
     public String loginHome(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member member,Model model){
+        addPostFormToModel(model);
         if(member==null){
             return "redirect:/";
         }
         model.addAttribute("member",member);
         return "loginHome";
+    }
+
+    private void addPostFormToModel(Model model) {
+        List<Post> posts = postService.findPosts(0, 10);
+        List<PostForm> postForms = posts.stream().map(o -> {
+            String content="";
+            if(o.getContent().length()>100){
+                content=o.getContent().substring(0,100);
+            }else{
+                content=o.getContent();
+            }
+            return new PostForm(o.getTitle(),content,o.getStatus(),o.getId());
+        }).collect(Collectors.toList());
+        model.addAttribute("postForms",postForms);
     }
 }
