56dff595ce1c4999a8e36c46921ef8236f5fea59
wogus <ind07152@naver.com>
2024-08-21 18:43:13 +0900

    feat: 홈 화면에서 포스트 출력 추가

--- src/main/java/project/general_project/web/controller/HomeController.java
+++ src/main/java/project/general_project/web/controller/HomeController.java
@@ -1,27 +1,35 @@
 package project.general_project.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttribute;
 import project.general_project.domain.Member;
 import project.general_project.domain.Post;
 import project.general_project.service.PostService;
 import project.general_project.web.SessionConst;
+import project.general_project.web.form.memberForm.Login;
 import project.general_project.web.form.postForm.PostForm;
+import project.general_project.web.form.postForm.PostSearchForm;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
     private final PostService postService;
 
     @GetMapping("/")
-    public String home(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member member,Model model){
-        addPostFormToModel(model);
+    public String home(@Login Member member, Model model, @ModelAttribute("postSearchForm") PostSearchForm postSearchForm, @RequestParam(name = "page",defaultValue = "1") Integer page){
+        addPostFormToModel(model,postSearchForm,page);
+        addPostCount(model,page);
+
         if(member==null){
             return "home";
         }
@@ -31,8 +39,10 @@ public class HomeController {
 
 
     @GetMapping("/loginHome")
-    public String loginHome(@SessionAttribute(name = SessionConst.LOGIN_MEMBER,required = false) Member member,Model model){
-        addPostFormToModel(model);
+    public String loginHome(@Login Member member,Model model,@ModelAttribute("postSearchForm") PostSearchForm postSearchForm,@RequestParam(name = "page",defaultValue = "1") Integer page){
+        addPostFormToModel(model,postSearchForm,page);
+        addPostCount(model,page);
+
         if(member==null){
             return "redirect:/";
         }
@@ -40,8 +50,8 @@ public class HomeController {
         return "loginHome";
     }
 
-    private void addPostFormToModel(Model model) {
-        List<Post> posts = postService.findPosts(0, 10);
+    private void addPostFormToModel(Model model,PostSearchForm postSearchForm,Integer page) {
+        List<Post> posts = postService.findPostsByTitle(postSearchForm.getContent(),page-1,10);
         List<PostForm> postForms = posts.stream().map(o -> {
             String content="";
             if(o.getContent().length()>100){
@@ -52,5 +62,20 @@ public class HomeController {
             return new PostForm(o.getTitle(),content,o.getStatus(),o.getId());
         }).collect(Collectors.toList());
         model.addAttribute("postForms",postForms);
+        model.addAttribute("postSearchForm",new PostSearchForm());
+        model.addAttribute("currentPage",page);
+    }
+
+    private void addPostCount(Model model,Integer page){
+        Long postCount = postService.getPostCount();
+        Long count=postCount/10;
+        if(postCount%10>0) count++;
+        model.addAttribute("count",count);
+        if((count-(page-1))>10){
+            model.addAttribute("scope",9);
+        }else{
+            log.info("count = {}",count-page);
+            model.addAttribute("scope",count-page);
+        }
     }
 }
