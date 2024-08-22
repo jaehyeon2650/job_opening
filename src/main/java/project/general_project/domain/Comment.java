7d3c7ba4e791b20fd484aca963d3a3a8a23348e6
wogus <ind07152@naver.com>
2024-08-22 18:29:15 +0900

    feat: comment 간편 연관 메서드, 생성 메서드 추가

--- src/main/java/project/general_project/domain/Comment.java
+++ src/main/java/project/general_project/domain/Comment.java
@@ -22,15 +22,30 @@ public class Comment {
     @JoinColumn(name="post_id")
     private Post post;
 
-    @ManyToOne(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Comment parent;
 
-    @OneToMany(mappedBy = "parent",orphanRemoval = true)
+    @OneToMany(mappedBy = "parent",orphanRemoval = true,cascade = CascadeType.ALL)
     private List<Comment> comments= new ArrayList<>();
     @Lob
     private String content;
 
     private LocalDateTime created;
-    private LocalDateTime updated;
+
+
+    public void addComment(Comment comment){
+        comments.add(comment);
+        comment.setParent(this);
+    }
+
+    public static Comment createComment(Member member,String content,Post post){
+        Comment comment=new Comment();
+        comment.setMember(member);
+        comment.setPost(post);
+        comment.setContent(content);
+        comment.setCreated(LocalDateTime.now());
+        return comment;
+    }
+
 }
