553c9c1283cca61f1d54103cfcba8d0c212a1ab4
wogus <ind07152@naver.com>
2024-09-06 18:49:28 +0900

    feat: 팀 수정 dto 추가

---
+++ src/main/java/project/general_project/web/form/teamForm/EditTeamForm.java
@@ -0,0 +1,36 @@
+package project.general_project.web.form.teamForm;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import project.general_project.domain.Team;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Getter
+public class EditTeamForm {
+    @NotBlank
+    private String teamName;
+    private String leaderUserId;
+    private List<String> members=new ArrayList<>();
+
+    public EditTeamForm(Team team){
+        this.teamName=team.getName();
+        this.leaderUserId=team.getLeader().getUserId();
+        team.getMembers().forEach(o->{
+            members.add(o.getUserId());
+        });
+    }
+
+    public EditTeamForm(String teamName, String leaderUserId, List<String> members) {
+        this.teamName = teamName;
+        this.leaderUserId = leaderUserId;
+        this.members = members;
+    }
+
+    public EditTeamForm() {
+    }
+}
