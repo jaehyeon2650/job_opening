100c87c8c6045e06b85a041404fbc28340637418
wogus <ind07152@naver.com>
2024-09-10 21:32:35 +0900

    feat: AssessmentRepository 추가

---
+++ src/main/java/project/general_project/repository/assessment/AssessmentRepository.java
@@ -0,0 +1,46 @@
+package project.general_project.repository.assessment;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.stereotype.Repository;
+import project.general_project.domain.Assessment;
+
+import java.util.List;
+
+import static project.general_project.domain.QAssessment.*;
+
+@Repository
+public class AssessmentRepository {
+    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    public AssessmentRepository(EntityManager em){
+        this.em=em;
+        query=new JPAQueryFactory(em);
+    }
+
+    public void save(Assessment assessment){
+        em.persist(assessment);
+    }
+
+    public double getAverageScore(Long memberId) {
+        Double result = (Double) em.createQuery("select avg(a.score) from Assessment a where a.toMember.id = :id", Double.class)
+                .setParameter("id", memberId)
+                .getSingleResult();
+        return result != null ? result : 0.0;
+    }
+
+    public Long getAssessmentCount(Long memberId){
+        Long count= (Long) em.createQuery("select count(a) from Assessment a where a.toMember.id=:id")
+                .setParameter("id",memberId)
+                .getSingleResult();
+        return count != null ? count : 0;
+    }
+
+    public List<Assessment> getAllAssessment(Long memberId){
+        return query.selectFrom(assessment)
+                .leftJoin(assessment.fromMember).fetchJoin()
+                .where(assessment.toMember.id.eq(memberId))
+                .fetch();
+    }
+}
