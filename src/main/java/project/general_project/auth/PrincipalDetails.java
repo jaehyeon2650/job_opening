685d077b4964cbe06f7b03f1d6739559511d9235
wogus <ind07152@naver.com>
2024-09-16 20:48:37 +0900

    feat: UserDetails 구현

---
+++ src/main/java/project/general_project/auth/PrincipalDetails.java
@@ -0,0 +1,35 @@
+package project.general_project.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import project.general_project.domain.Member;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter @Setter
+public class PrincipalDetails implements UserDetails {
+
+    private final Member member;
+
+    public PrincipalDetails(Member member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getUsername();
+    }
+}
