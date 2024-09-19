b673bfa5c7cd90e07637418e77a38f3797488784
wogus <ind07152@naver.com>
2024-09-19 13:02:47 +0900

    feat: PrincipalDetails Oauth2User 상속 추가

--- src/main/java/project/general_project/auth/PrincipalDetails.java
+++ src/main/java/project/general_project/auth/PrincipalDetails.java
@@ -4,20 +4,39 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import project.general_project.domain.Member;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Getter @Setter
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private final Member member;
+    private final Map<String,Object> attributes;
+
+    public PrincipalDetails(Member member, Map<String, Object> attributes) {
+        this.member = member;
+        this.attributes = attributes;
+    }
 
     public PrincipalDetails(Member member) {
         this.member = member;
+        this.attributes=null;
     }
 
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return List.of();
@@ -32,4 +51,5 @@ public class PrincipalDetails implements UserDetails {
     public String getUsername() {
         return member.getUserId();
     }
+
 }
